3929d8d87802533204b1a14a170215d21c1d847d
hgouchet <rvflash@users.noreply.github.com>
2019-03-19 00:07:48 +0100

    adds tests on Any

--- server_test.go
+++ server_test.go
@@ -1,7 +1,11 @@
 package tcp_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
+
+	"github.com/matryer/is"
 
 	"github.com/rvflash/tcp"
 )
@@ -16,4 +20,44 @@ func TestDefault(t *testing.T) {
 	srv := tcp.Default()
 	srv.ACK(stumble)
 	// now runs it!
+}
+
+func TestServer_Any(t *testing.T) {
+	var (
+		any = handleResp(tcp.ANY, false)
+		dt  = []struct {
+			req *tcp.Request
+			out string
+		}{
+			{req: tcp.NewRequest(tcp.ANY, nil), out: any + handleResp(tcp.SYN, true)},
+			{req: tcp.NewRequest(tcp.ACK, nil), out: any + handleResp(tcp.ACK, true)},
+			{req: tcp.NewRequest(tcp.FIN, nil), out: any + handleResp(tcp.FIN, true)},
+			{req: tcp.NewRequest(tcp.SYN, nil), out: any + handleResp(tcp.SYN, true)},
+		}
+		are = is.New(t)
+	)
+	// Listens all segments
+	srv := tcp.New()
+	for _, seg := range []string{tcp.ANY, tcp.ACK, tcp.FIN, tcp.SYN} {
+		srv.Any(seg, handle(seg))
+	}
+	// Launches the test cases
+	for i, tt := range dt {
+		tt := tt
+		t.Run("#"+strconv.Itoa(i), func(t *testing.T) {
+			rec := tcp.NewRecorder()
+			srv.ServeTCP(rec, tt.req)
+			are.Equal(rec.Body.String(), tt.out)
+		})
+	}
+}
+
+func handle(segment string) tcp.HandlerFunc {
+	return func(c *tcp.Context) {
+		c.String(handleResp(segment, c.Request.Segment == segment))
+	}
+}
+
+func handleResp(segment string, expected bool) string {
+	return fmt.Sprintf("%q segment: %t\n", segment, expected)
 }
